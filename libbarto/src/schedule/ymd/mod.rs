391fda114236e228e9481d6bae5a3a5346c2db52
Jason Ozias <jason.g.ozias@gmail.com>
2025-10-19 19:21:02 -0400

    Days testing coverage, YMD testing coverage

--- libbarto/src/schedule/ymd/mod.rs
+++ libbarto/src/schedule/ymd/mod.rs
@@ -6,6 +6,7 @@
 // option. All files in the project carrying such notice may not be copied,
 // modified, or distributed except according to those terms.
 
+pub(crate) mod day;
 pub(crate) mod month;
 pub(crate) mod year;
 
@@ -14,95 +15,13 @@ use std::fmt::{Display, Formatter};
 use anyhow::{Error, Result};
 use bon::Builder;
 use getset::{CopyGetters, Getters};
-use rand::Rng;
 
 use crate::{
-    error::Error::{InvalidDate, InvalidDay},
-    schedule::{
-        All, parse_time_chunk,
-        ymd::{month::Month, year::Year},
-    },
-    utils::as_two_digit,
+    error::Error::InvalidDate,
+    schedule::ymd::{day::Day, month::Month, year::Year},
 };
 
 // TODO: Fix this
-const DAYS_PER_MONTH: u8 = 31;
-const DAYS_PER_MONTH_RAND: u8 = 28;
-
-/// The date for a realtime schedule
-#[derive(Clone, Debug, Default, Eq, Hash, PartialEq)]
-pub enum Day {
-    /// Every day
-    #[default]
-    All,
-    /// Specific days
-    Days(Vec<u8>),
-}
-
-impl Day {
-    pub(crate) fn matches(&self, given: u8) -> bool {
-        match self {
-            Day::All => true,
-            Day::Days(days) => days.contains(&given),
-        }
-    }
-
-    pub(crate) fn first() -> Self {
-        Day::Days(vec![1])
-    }
-}
-
-impl All for Day {
-    fn all() -> Self {
-        Self::All
-    }
-
-    fn rand() -> Self {
-        let mut rng = rand::rng();
-        let rand_in_range = rng.random_range(1..=DAYS_PER_MONTH_RAND);
-        Day::Days(vec![rand_in_range])
-    }
-}
-
-impl TryFrom<Vec<u8>> for Day {
-    type Error = Error;
-
-    fn try_from(values: Vec<u8>) -> Result<Self> {
-        for &value in &values {
-            if value == 0 || value > DAYS_PER_MONTH {
-                return Err(InvalidDay(value.to_string()).into());
-            }
-        }
-        Ok(Day::Days(values))
-    }
-}
-
-impl TryFrom<u8> for Day {
-    type Error = Error;
-
-    fn try_from(value: u8) -> Result<Self> {
-        Day::try_from(vec![value])
-    }
-}
-
-impl TryFrom<&str> for Day {
-    type Error = Error;
-
-    fn try_from(dayish: &str) -> Result<Self> {
-        parse_time_chunk::<Day>(dayish, DAYS_PER_MONTH, true)
-    }
-}
-
-impl Display for Day {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Day::All => write!(f, "*"),
-            Day::Days(days) => {
-                write!(f, "{}", as_two_digit(days))
-            }
-        }
-    }
-}
 
 /// A year month day combinations
 #[derive(Builder, Clone, CopyGetters, Debug, Default, Eq, Getters, Hash, PartialEq)]
@@ -178,5 +97,25 @@ impl TryFrom<&str> for YearMonthDay {
 impl Display for YearMonthDay {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "{} {} {}", self.year, self.month, self.day)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{Day, Month, Year};
+
+    use super::YearMonthDay;
+
+    use anyhow::Result;
+
+    #[test]
+    fn display_works() -> Result<()> {
+        let ymd = YearMonthDay::builder()
+            .year(Year::try_from("2025")?)
+            .month(Month::first())
+            .day(Day::try_from(15)?)
+            .build();
+        assert_eq!(ymd.to_string(), "2025 01 15");
+        Ok(())
     }
 }
